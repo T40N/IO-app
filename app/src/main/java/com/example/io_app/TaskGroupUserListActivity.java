e3f632b0803a606f86ba2bc6c262be55ba0eb7dc
pbalcer <piotrbalcer@gmail.com>
2021-05-16 18:31:23 +0200

    Tasks group - dodano widoki dla listy grup zadaniowych, listy uczestnikow, listy   zadan - stworzono widoki dla delegatow list: uczestnikow grup i zadan w grupie - dodano style dla tekstow o rozmiarze 12sp i 18sp - zdefiniowano nowe parametry: margin24, padding, radius, rozmiary   tekstow dla medium i small - dodano nowe ikony

--- app/src/main/java/com/example/io_app/TaskGroupUserListActivity.java
+++ app/src/main/java/com/example/io_app/TaskGroupUserListActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TaskGroupActivity extends AppCompatActivity {
+public class TaskGroupUserListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task_group);
+        setContentView(R.layout.activity_task_group_user_list);
     }
 }
