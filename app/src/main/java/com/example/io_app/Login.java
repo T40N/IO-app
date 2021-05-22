cab3947a32137d86a07c0db314cb792b976580f2
t40n <b.ratajczyk124@gmail.com>
2021-05-22 14:53:14 +0200

    firebase login problem

--- app/src/main/java/com/example/io_app/Login.java
+++ app/src/main/java/com/example/io_app/Login.java
@@ -2,13 +2,32 @@ package com.example.io_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_view);
+
+        register = (Button) findViewById(R.id.register_btn_ID);
+        register.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register_btn_ID:
+                startActivity(new Intent(this, Register.class));
+                break;
+        }
     }
 }
