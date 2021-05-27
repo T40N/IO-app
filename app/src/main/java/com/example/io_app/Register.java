2c60fb95937623098a8b4ab2cd520af535bdcc81
J-sob <sobczynski.kuba@wp.pl>
2021-05-27 18:12:09 +0200

    User profile data fill

--- app/src/main/java/com/example/io_app/Register.java
+++ app/src/main/java/com/example/io_app/Register.java
@@ -16,6 +16,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -119,8 +121,11 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
                                 @Override
                                 public void onComplete(@NonNull @NotNull Task<Void> task) {
                                     if(task.isSuccessful()){
-                                        Toast.makeText(Register.this, "Użtkownik został zarejestrowany!", Toast.LENGTH_LONG).show();
+                                        Toast.makeText(Register.this, "Użytkownik został zarejestrowany!", Toast.LENGTH_LONG).show();
                                         progressBar.setVisibility(View.GONE);
+                                        FirebaseUser FBuser = FirebaseAuth.getInstance().getCurrentUser();
+                                        UserProfileChangeRequest userUpdate = new UserProfileChangeRequest.Builder().setDisplayName(user.getName() + " " + user.getSurname()).build();
+                                        FBuser.updateProfile(userUpdate);
                                         startActivity(new Intent(Register.this, Login.class));
                                     }else{
                                         Toast.makeText(Register.this, "Nie udało się zarejestrować! Spróbuj jeszcze raz!", Toast.LENGTH_LONG).show();
