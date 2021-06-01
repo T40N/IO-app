788b554ae687084c3c1b1e9d8228dd8c3b0cbdf2
Bartosz Ratajczyk <t40n@pop-os.localdomain>
2021-06-01 05:47:23 +0200

    dada

--- app/src/main/java/com/example/io_app/AddTask.java
+++ app/src/main/java/com/example/io_app/AddTask.java
@@ -1,13 +1,78 @@
 package com.example.io_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class AddTask extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class AddTask extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_task_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        navigationView = findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.toolbar);
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open,R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.main_view:
+                Intent intent = new Intent(AddTask.this,UserProfile.class);
+                startActivity(intent);
+                break;
+            case R.id.calendar:
+                intent = new Intent(AddTask.this,Calendar.class);
+                startActivity(intent);
+                break;
+            case R.id.groups:
+                intent = new Intent(AddTask.this, AddGroup.class);
+                startActivity(intent);
+                break;
+            case R.id.tasks:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            case R.id.chat:
+                intent = new Intent(AddTask.this, ChatList.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/io_app/ChatList.java
+++ app/src/main/java/com/example/io_app/ChatList.java
@@ -1,18 +1,33 @@
 package com.example.io_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class ChatList extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ChatList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     ImageButton addButton;
 
     RecyclerView personalRecyclerView;
     RecyclerView groupRecyclerView;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,20 @@ public class ChatList extends AppCompatActivity {
         personalRecyclerView = findViewById(R.id.personalMessageRecyclerView);
         groupRecyclerView = findViewById(R.id.groupMessageRecyclerView);
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        navigationView = findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.toolbar);
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open,R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,5 +59,42 @@ public class ChatList extends AppCompatActivity {
                 System.out.println("Adding new chat");
             }
         });
+    }
+
+    @Override
+    public void onBackPressed(){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.main_view:
+                Intent intent = new Intent(ChatList.this,UserProfile.class);
+                startActivity(intent);
+                break;
+            case R.id.calendar:
+                intent = new Intent(ChatList.this,Calendar.class);
+                startActivity(intent);
+                break;
+            case R.id.groups:
+                intent = new Intent(ChatList.this, AddGroup.class);
+                startActivity(intent);
+                break;
+            case R.id.tasks:
+                intent = new Intent(ChatList.this, AddTask.class);
+                startActivity(intent);
+                break;
+            case R.id.chat:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+            default:
+                break;
+        }
+        return false;
     }
 }
